b282b74a7201aee6463b9eda8887694ee3f6c4c8
jinfei <ascode@icloud.com>
2016-01-06 13:03:19 +0800

    init

---
+++ jif-SwiftSyntax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jif-SwiftSyntax
+//
+//  Created by 金飞 on 16/1/6.
+//  Copyright © 2016年 Fei Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jif-SwiftSyntaxTests/jif_SwiftSyntaxTests.swift
@@ -0,0 +1,36 @@
+//
+//  jif_SwiftSyntaxTests.swift
+//  jif-SwiftSyntaxTests
+//
+//  Created by 金飞 on 16/1/6.
+//  Copyright © 2016年 Fei Jin. All rights reserved.
+//
+
+import XCTest
+@testable import jif_SwiftSyntax
+
+class jif_SwiftSyntaxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
